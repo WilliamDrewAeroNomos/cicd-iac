26d98bc795139226d4cd6191dfb571f324915183
William Drew <wdrew@governmentcio.com>
2022-03-17 10:23:31 -0400

    Updated node.js setup

--- cicd/packer/ahroc-nodejs-setup.sh
+++ cicd/packer/ahroc-nodejs-setup.sh
@@ -8,6 +8,13 @@ echo "Update the image to the latest patches"
 sudo yum install epel-release -y
 sudo yum update -y
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install node
+
+node -e "console.log('Running Node.js ' + process.version)"
 
 END_TIMESTAMP=`date +"%Y-%m-$d %T"`
 echo End time : ${END_TIMESTAMP}
