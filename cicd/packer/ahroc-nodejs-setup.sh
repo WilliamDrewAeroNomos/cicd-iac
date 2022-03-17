3993291953fe425326d248fec02c02ba3d5f9b7e
William Drew <wdrew@governmentcio.com>
2022-03-17 15:57:19 -0400

    Updated setup script

--- cicd/packer/ahroc-nodejs-setup.sh
+++ cicd/packer/ahroc-nodejs-setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "Executing base image setup"
 
-START_TIMESTAMP=`date +"%Y-%m-$d %T"`
-echo Start time : ${START_TIMESTAMP}
-
 echo "Update the image to the latest patches"
+
 sudo yum install epel-release -y
 sudo yum update -y
 
@@ -16,5 +14,13 @@ nvm install node
 
 node -e "console.log('Running Node.js ' + process.version)"
 
-END_TIMESTAMP=`date +"%Y-%m-$d %T"`
-echo End time : ${END_TIMESTAMP}
+sudo yum install git -y
+
+git clone https://github.com/WilliamDrewAeroNomos/test-node-app.git
+
+cd test-node-app
+ 
+npm install
+ 
+node index.js
+ 
